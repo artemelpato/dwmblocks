e8077cb43f3d32bb8d89938d93229eb58d66cfc5
artem <artemselezenev@mail.ru>
2024-02-24 16:53:10 +0300

    separate blocks for time and date

--- blocks.def.h
+++ blocks.def.h
@@ -4,7 +4,8 @@ static const Block blocks[] = {
     {"", "volume_status", 10, 10},
     {"", "capacity", 60, 0},
     {"", "connection_status", 30, 0},
-	{"󱑆 ", "date '+%b %d (%a) %I:%M%p'", 5, 0},
+	{"󰃶 ", "date '+%b %d (%a)'", 60, 0},
+	{"󱑆 ", "date '+%H:%M'", 10, 0},
     {" ", "", 60, 0}
 };
 
